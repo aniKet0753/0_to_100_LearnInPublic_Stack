cd709f1a62b48c37913503794375fbb7d82339a8
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2025-07-05 22:23:36 +0530

    "EXPRESS LEARNING"

--- express/javascrit.express.js
+++
@@ -1,6 +0,0 @@
-//what is htpp 
-//htpp is the way to communicate to the server y sending http request on server then the server send me back something 
-//lke suppose we are hitting the google web site inn backend side we are hittind the google server and asking for goggle frontends that hey we visited your website 
-//give me000e html css
-//cliend taking with server is called request responce model
-// there are other ways to communecate between machine like websocket webrtc or grpoc 
